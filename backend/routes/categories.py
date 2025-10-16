bc36610dd1ca03a5d9064a76b50aee70b1b95bc6
Abdimalik123 <abdi9626@hotmail.com>
2025-10-16 11:58:08 +0200

    Finished categories route

--- backend/routes/categories.py
+++ backend/routes/categories.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, redirect, session, url_for
+from flask import Blueprint, g, jsonify
 from routes.auth import login_required
+from db import conn
 
 
 
@@ -7,6 +8,35 @@ from routes.auth import login_required
 category_bp = Blueprint('category_bp', __name__)
 
 
-category_bp.route('/', methods = ['GET'])
+@category_bp.route('/categories', methods = ['GET'])
 def all_categories():
-    return "Coming soon"
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM categories")
+    rows = cursor.fetchall()
+    cursor.close()
+    categories = [row[0] for row in rows]
+    return jsonify({"Categories": categories})
+
+@category_bp.route('/categories/<int:category_id>/recipes', methods = ['GET'])
+def get_recipes(category_id):
+    
+    try:
+        cursor = conn.cursor()
+        cursor.execute("SELECT r.title, u.first_name, u.last_name FROM recipes r JOIN recipe_categories rc ON r.id = rc.recipe_id JOIN users u ON r.user_id = u.id WHERE rc.category_id = %s", (category_id,))
+        all_recipe_info = cursor.fetchall()
+        cursor.close()
+
+        if not all_recipe_info:
+            return jsonify({"message": "No recipes found for this category"}), 404
+        
+        info = [
+            {
+                "title":row[0],
+                "firstname":row[1],
+                "lastname":row[2]
+            }
+            for row in all_recipe_info
+        ]
+        return jsonify({"info": info}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
